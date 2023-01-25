19255305e13df6171425218d444b63344eb78be1
ninedev <master-vva@narod.ru>
2023-01-25 10:33:42 +0300

    add event model

--- app/Models/Event.ts
+++ app/Models/Event.ts
@@ -5,14 +5,19 @@ export default class Event extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
   public title: string
 
+  @column()
   public description: string
 
+  @column()
   public geo: string
 
+  @column()
   public owner: number
 
+  @column()
   public is_public: boolean
 
   @column.dateTime({ autoCreate: true })
