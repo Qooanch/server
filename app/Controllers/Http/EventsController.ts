dd6b58c3613446ce03a0d94e18b0343ca7200c87
akuptsova <akuptsova@korusconsulting.ru>
2023-01-30 20:29:50 +0300

    add events routes

---
+++ app/Controllers/Http/EventsController.ts
@@ -0,0 +1,37 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Event from 'App/Models/Event'
+
+export default class EventsController {
+  public async getEvents() {
+    return Event.query()
+  }
+
+  public async getEvent({ params }: HttpContextContract) {
+    const id = params.eventId
+
+    return await Event
+      .query()
+      .where('id', id)
+      .first();
+  }
+
+  public async createEvent({ request }: HttpContextContract) {
+    const data = { ...request.all() }
+    await Event.create(data)
+    return this.getEvents()
+  }
+
+  public async deleteEvent({ params }: HttpContextContract) {
+    return Event
+      .query()
+      .where('id', params.eventId)
+      .delete()
+  }
+
+  public async editEvent({ params, request }: HttpContextContract) {
+    return Event
+      .query()
+      .where('id', params.eventId)
+      .update(request.all())
+  }
+}
